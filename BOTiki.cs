6729542be1d3c79e1851503cb5910e7afb25f746
jockii <konon3nko98@gmail.com>
2023-11-14 12:49:43 +0200

    add_config

--- BOTiki.cs
+++ BOTiki.cs
@@ -1,29 +1,52 @@
-﻿using CounterStrikeSharp.API;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
-using CounterStrikeSharp.API.Modules.Cvars;
-using CounterStrikeSharp.API.Modules.Entities;
 using CounterStrikeSharp.API.Modules.Utils;
 
 namespace BOTiki;
-public class BOTiki : BasePlugin
+public class BOTiki : BasePlugin, IPluginConfig<BOTikiConfig>
 {
     public override string ModuleName => "BOTiki";
 
-    public override string ModuleVersion => "0.0.1";
+    public override string ModuleVersion => "0.2";
 
     public override string ModuleAuthor => "jockii, VoCs";
 
+    public BOTikiConfig Config { get; set; } = new();
+    private string _BotMode = "";
+    private int _bot_count = 0;
+    private int _max_player_to_bot_kick = 0;
     public override void Load(bool hotReload)
     {
         Console.WriteLine("----------------------------------------------------");
         Console.WriteLine($"Plugin: {ModuleName} ver:{ModuleVersion} by {ModuleAuthor} has been loaded =)");
         Console.WriteLine("---------------------------------------------");
-
+        /* --------- create a json config -----------
+        var configPath = Path.Join(ModuleDirectory, "Config.json");
+        if (!File.Exists(configPath))
+        {
+            var data = new Config() { BotMode = "balanced", bot_count = 10, max_player_to_bot_kick = 10 };
+            File.WriteAllText(configPath, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping }));
+            _config = data;
+        }
+        else _config = JsonSerializer.Deserialize<Config>(File.ReadAllText(configPath));
+        */
+        
         Server.ExecuteCommand("sv_cheats true");
+        Server.ExecuteCommand("bot_join_after_player true");
         Server.ExecuteCommand("bot_quota 1");
         Server.ExecuteCommand("bot_quota_mode match");
         Server.ExecuteCommand("sv_cheats false");
+    }
+
+    public void OnConfigParsed(BOTikiConfig config)
+    {
+        this.Config = config;
+        _BotMode = config.BotMode;
+        _bot_count = config.bot_count;
+        _max_player_to_bot_kick = config.max_player_to_bot_kick;
     }
 
     const string BOT_ADD_CT = "bot_add_ct";
@@ -60,6 +83,8 @@ public class BOTiki : BasePlugin
         realPlayers.Find(player => player.TeamNum == teamToChange).ChangeTeam(teamName);
     }
 
+    /*    ------ backup --------
+         
     public void AddBotsByPlayersCount(int T, int CT)
     {
         if (T + CT == 1)
@@ -75,10 +100,65 @@ public class BOTiki : BasePlugin
             Server.ExecuteCommand(T > CT ? BOT_ADD_CT : BOT_ADD_T);
         }
     }
+    */
 
-    public void KickBotsByPlayersCount(int T, int CT)
+    public void AddBotsByConfigMode(int T, int CT, string _BotMode, int _bot_count)
     {
-        if (T + CT == 2 || T + CT > 3)
+        switch (_BotMode)
+        {
+            case "fill":
+                // log
+                Server.PrintToChatAll("--- fill case ---");/////////////
+                Console.WriteLine("--- fill case ---");/////////////////
+                //
+                Server.ExecuteCommand($"bot_quota {_bot_count}");
+                Server.ExecuteCommand("bot_quota_mode fill"); break;
+            case "match":
+                // log
+                Server.PrintToChatAll("--- match case ---");/////////////
+                Console.WriteLine("--- match case ---");////////////////
+                //
+                Server.ExecuteCommand($"bot_quota 1");
+                Server.ExecuteCommand("bot_quota_mode match"); break;
+            case "balanced":
+                // log
+                Server.PrintToChatAll("--- balanced case ---");///////////////
+                Console.WriteLine("--- balanced case ---");//////////////////
+                //
+                if (T + CT == 1)
+                {
+                    Server.ExecuteCommand("bot_quota_mode match");
+                    Server.ExecuteCommand("bot_join_after_player true");
+                    Server.ExecuteCommand(T == 1 ? BOT_ADD_CT : BOT_ADD_T);
+                }
+                else if (T + CT == 3 ) // (T + CT) % 2
+                {
+                    Server.ExecuteCommand("bot_quota_mode fill");
+                    Server.ExecuteCommand("bot_join_after_player true");
+                    Server.ExecuteCommand(T > CT ? BOT_ADD_CT : BOT_ADD_T);
+                } break;
+            default:
+                Console.WriteLine("------------------------------------------------(");
+                Console.WriteLine("'switch' not working =(");
+                Console.WriteLine("'switch' not working =(");
+                Console.WriteLine("'switch' not working =(");
+                Console.WriteLine("'switch' not working =(");
+                Console.WriteLine("------------------------------------------------(");
+                break;
+        }
+    }
+
+    public void KickBotsByPlayersCount(int T, int CT, int _max_player_to_bot_kick)
+    {
+        /*
+         *  ----------- backup -----------
+            if (T + CT == 2 || T + CT > 3) 
+            Server.ExecuteCommand(BOT_KICK);
+         */
+
+        if (T == CT)
+            Server.ExecuteCommand(BOT_KICK);
+        else if (T + CT == _max_player_to_bot_kick)
             Server.ExecuteCommand(BOT_KICK);
     }
     public void Checker(List<CCSPlayerController> players)
@@ -98,9 +178,9 @@ public class BOTiki : BasePlugin
         });
 
         if (isBotExists)
-            KickBotsByPlayersCount(T, CT);
+            KickBotsByPlayersCount(T, CT, _max_player_to_bot_kick);
         else
-            AddBotsByPlayersCount(T, CT);
+            AddBotsByConfigMode(T, CT, _BotMode, _bot_count);
 
         if (T > 1 && CT == 0)
             ChangePlayerTeamSide(realPlayers, CsTeam.Terrorist);
---
+++ BOTikiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using CounterStrikeSharp.API.Core;
+
+namespace BOTiki;
+
+public class BOTikiConfig : BasePluginConfig
+{
+
+    [JsonPropertyName("BotMode")]
+    public string BotMode { get; set; } = "balanced"; // fill, match, balanced
+
+    [JsonPropertyName("bot_count")]
+    public int bot_count { get; set; } = 10; // uses only fill mode
+
+    [JsonPropertyName("max_player_to_bot_kick")]
+    public int max_player_to_bot_kick { get; set; } = 10; // after this player count all bot_kick
+}
