259d347d0323f3895dbb4f3d36ed8959d2fbc0e5
jockii <konon3nko98@gmail.com>
2023-11-15 13:09:22 +0200

    api v53 update

--- BOTiki.cs
+++ BOTiki.cs
@@ -2,14 +2,13 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Utils;
-using System.Collections.Generic;
 
 namespace BOTiki;
 public class BOTiki : BasePlugin, IPluginConfig<BOTikiConfig>
 {
     public override string ModuleName => "BOTiki";
 
-    public override string ModuleVersion => "v0.2";
+    public override string ModuleVersion => "v0.3";
 
     public override string ModuleAuthor => "jockii, VoCs";
 
@@ -25,8 +24,8 @@ public class BOTiki : BasePlugin, IPluginConfig<BOTikiConfig>
 
         Server.ExecuteCommand("sv_cheats true");
         Server.ExecuteCommand("bot_join_after_player true");
-        Server.ExecuteCommand("bot_quota 1");
-        Server.ExecuteCommand("bot_quota_mode match");
+        Server.ExecuteCommand("bot_quota 0");
+        Server.ExecuteCommand("bot_quota_mode normal");
         Server.ExecuteCommand("sv_cheats false");
     }
 
@@ -41,17 +40,16 @@ public class BOTiki : BasePlugin, IPluginConfig<BOTikiConfig>
     const string BOT_ADD_CT = "bot_add_ct";
     const string BOT_ADD_T = "bot_add_t";
     const string BOT_KICK = "bot_kick";
-    const string BOT_AUTO_VACATE = "bot_auto_vacate 1";
 
     List<CCSPlayerController> players = Utilities.GetPlayers();
 
     public void ChangePlayerTeamSide(List<CCSPlayerController> realPlayers, CsTeam teamName)
     {
         int teamToChange = teamName == CsTeam.Terrorist ? 3 : 2;
-        realPlayers.Find(player => player.TeamNum == teamToChange).ChangeTeam(teamName);
+        realPlayers.Find(player => player.TeamNum == teamToChange)?.ChangeTeam(teamName); //////////// line 51: null => ?
     }
 
-    public void AddBotsByConfigMode(int T, int CT, bool isBotExists) // , string _BotMode, int _bot_count
+    public void AddBotsByConfigMode(int T, int CT, bool isBotExists)
     {
         if (isBotExists && T + CT >= _max_player_to_bot_kick)
             Server.ExecuteCommand(BOT_KICK);
@@ -59,37 +57,31 @@ public class BOTiki : BasePlugin, IPluginConfig<BOTikiConfig>
         {
             switch (_BotMode)
             {
-                case "fill":
+                case "normal":
                     Server.ExecuteCommand(BOT_KICK);
                     Server.ExecuteCommand($"bot_quota {_bot_count}");
-                    Server.ExecuteCommand("bot_quota_mode fill");
-                    Server.ExecuteCommand(BOT_AUTO_VACATE); break;
-                case "match":
-                    Server.ExecuteCommand(BOT_KICK);
-                    Server.ExecuteCommand($"bot_quota 1");
-                    Server.ExecuteCommand("bot_quota_mode match"); break;
+                    Server.ExecuteCommand("bot_quota_mode normal"); break;
                 case "balanced":
                     if (T + CT == 1)
                     {
                         Server.ExecuteCommand(BOT_KICK);
                         Server.ExecuteCommand("bot_quota_mode match");
+                        Server.ExecuteCommand("bot_quota 1");
                         Server.ExecuteCommand("bot_join_after_player true");
                         Server.ExecuteCommand(T == 1 ? BOT_ADD_CT : BOT_ADD_T);
                     }
-                    else if (T != CT)
+                    else if (T != CT && !isBotExists)
                     {
                         Server.ExecuteCommand(BOT_KICK);
-                        Server.ExecuteCommand("bot_quota_mode fill");
+                        Server.ExecuteCommand("bot_quota_mode normal");
+                        Server.ExecuteCommand("bot_quota 1");
                         Server.ExecuteCommand("bot_join_after_player true");
                         Server.ExecuteCommand(T > CT ? BOT_ADD_CT : BOT_ADD_T);
                     }
                     break;
                 default:
                     Console.WriteLine("------------------------------------------------(");
-                    Console.WriteLine("'switch' not working =(");
-                    Console.WriteLine("'switch' not working =(");
-                    Console.WriteLine("'switch' not working =(");
-                    Console.WriteLine("'switch' not working =(");
+                    Console.WriteLine("'switch Plugin' not working =(");
                     Console.WriteLine("------------------------------------------------(");
                     break;
             }
@@ -112,7 +104,7 @@ public class BOTiki : BasePlugin, IPluginConfig<BOTikiConfig>
                 CT++;
         });
 
-        AddBotsByConfigMode(T, CT, isBotExists); // , _BotMode, _bot_count
+        AddBotsByConfigMode(T, CT, isBotExists);
 
         if (T > 1 && CT == 0)
             ChangePlayerTeamSide(realPlayers, CsTeam.Terrorist);
@@ -139,10 +131,10 @@ public class BOTiki : BasePlugin, IPluginConfig<BOTikiConfig>
         return HookResult.Continue;
     }
     [GameEventHandler]
-    public HookResult OnPlayerTeamChange(EventSwitchTeam @event, GameEventInfo info)
+    public HookResult OnPlayerSwitchTeam(EventSwitchTeam @event, GameEventInfo info, bool isBotExists)
     {  
-        List<CCSPlayerController> alivePlayers = Utilities.GetPlayers().FindAll(player => !player.IsBot && player.PawnIsAlive);
-        if (alivePlayers.Count < 2)
+        List<CCSPlayerController> realPlayers = Utilities.GetPlayers().FindAll(player => !player.IsBot && player.IsValid);
+        if (realPlayers.Count == 1)
         {
             Server.ExecuteCommand("sv_cheats true");
             Server.ExecuteCommand("endround");
--- BOTikiConfig.cs
+++ BOTikiConfig.cs
@@ -6,7 +6,7 @@ namespace BOTiki;
 public class BOTikiConfig : BasePluginConfig
 {
     [JsonPropertyName("BotMode")]
-    public string BotMode { get; set; } = "balanced"; // fill, match, balanced
+    public string BotMode { get; set; } = "normal"; // normal, balanced
 
     [JsonPropertyName("bot_count")]
     public int bot_count { get; set; } = 1; // used only fill mode
